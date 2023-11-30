f5552dffee4625d70151dee63c0ebb018fe87d1d
get6 <ittae.com@gmail.com>
2023-11-30 19:21:03 +0900

    Change name Post to ListPost

--- app/ui/home/ListPost.tsx
+++ app/ui/home/ListPost.tsx
@@ -1,13 +1,15 @@
 import Tag from '@/app/ui/Tag'
+import PostDate from '@/app/ui/home/post/PostDate'
+import PostTitle from '@/app/ui/home/post/PostTitle'
 
-export default function Post() {
+export default function ListPost() {
   return (
     <div className="flex h-[205px] w-[520px] items-start justify-start border border-black bg-white">
       <div className="h-[205px] w-[164px]">{/* 이미지 자리 */}</div>
       <div className="flex flex-col items-start justify-center gap-4 self-stretch border-l border-black px-6 py-2">
         <div className="flex w-full flex-col gap-2">
-          <div className="text-xs font-normal">Mar 23・2 min</div>
-          <div className="text-2xl font-bold">Title here</div>
+          <PostDate date={'Mar 23'} readingTime={2} />
+          <PostTitle>Title here</PostTitle>
           <div className="font-normal">Descriptions</div>
         </div>
         <div className="inline-flex items-start justify-start gap-2 py-2">
