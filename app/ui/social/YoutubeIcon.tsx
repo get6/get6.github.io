2da2eb4ab1aa12148721a0d09b47f681e8f03014
get6 <ittae.com@gmail.com>
2024-01-02 13:25:30 +0900

    Add fill to Icons

--- app/ui/social/YoutubeIcon.tsx
+++ app/ui/social/YoutubeIcon.tsx
@@ -6,8 +6,6 @@ export default function YoutubeIcon({ className }: Props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
       fill="currentColor"
       className={className}
       viewBox="0 0 16 16"
