f40b74a57038397fc5eddbc7fe7efc922ce5e4a1
get6 <ittae.com@gmail.com>
2024-01-18 23:59:09 +0900

    Add GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,17 @@ import { Providers } from '@/app/providers'
 import Navbar from '@/app/ui/Navbar'
 import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.NODE_ENV === 'production'
+      ? 'https://get6.github.io'
+      : 'http://localhost:3000',
+  ),
   title: blog_title,
   description: blog_description,
   icons: [
@@ -40,6 +46,21 @@ export default function RootLayout({
           </header>
           {children}
         </Providers>
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            {/* Google tag (gtag.js) */}
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-YGW2B2X46K" />
+            <Script id="google-analytics">
+              {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-YGW2B2X46K');
+              `}
+            </Script>
+          </>
+        )}
       </body>
     </html>
   )
--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -5,10 +5,20 @@ import Logo from '@/app/ui/Logo'
 import ThemeToggle from '@/app/ui/home/ThemeToggle'
 
 import { HashtagIcon } from '@heroicons/react/20/solid'
+import { Bars3Icon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useState } from 'react'
 
 export default function Navbar() {
+  // State to keep track of whether the menu is open
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  // Function to toggle the menu state
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   const pathname = usePathname()
 
   const menus = [
@@ -19,8 +29,8 @@ export default function Navbar() {
     { name: 'About me', href: '/about' },
   ]
 
-  return (
-    <nav className="flex h-8 items-center justify-start divide-x divide-black border border-black bg-white dark:divide-white dark:border-white dark:bg-gray-900">
+  const mdNavbar = (
+    <>
       <div className="px-8 dark:text-white sm:px-2 md:px-2 lg:px-3">
         <Link
           href="/"
@@ -53,6 +63,69 @@ export default function Navbar() {
         <a href="https://ittae.com/" target="_blank">
           <HashtagIcon className="h-5 w-5 text-gray-900 hover:cursor-pointer hover:text-blue-700 dark:text-white dark:hover:text-blue-500" />
         </a>
+      </div>
+    </>
+  )
+
+  const defaultNavbar = (
+    <>
+      <div className="flex h-full items-center px-2 hover:cursor-pointer dark:text-white">
+        <button
+          type="button"
+          className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+          onClick={toggleMenu}
+        >
+          <Bars3Icon className="h-6 w-6 dark:text-white" />
+        </button>
+      </div>
+      <div
+        className={`${
+          isMenuOpen ? 'flex' : 'hidden'
+        } absolute left-2 top-10 z-10 md:block`}
+      >
+        <div className=" w-full items-center justify-between md:order-1 md:flex md:w-auto">
+          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
+            {menus.map((menu, index) => (
+              <li key={index}>
+                <Link
+                  href={menu.href}
+                  className={`
+                    ${
+                      pathname === menu.href &&
+                      'block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500'
+                    }
+                    ${
+                      pathname !== menu.href &&
+                      'block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500'
+                    }`}
+                >
+                  {menu.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <Link
+        href="/"
+        className="flex h-full items-center justify-center gap-2 text-xl font-light hover:cursor-pointer dark:text-white"
+      >
+        <Logo />
+        {blog_title}
+      </Link>
+      <div className="flex h-full items-center px-4">
+        <ThemeToggle />
+      </div>
+    </>
+  )
+
+  return (
+    <nav className="bg-white dark:bg-gray-900">
+      <div className="hidden h-8 items-center justify-start divide-x divide-black border border-black dark:divide-white dark:border-white md:flex">
+        {mdNavbar}
+      </div>
+      <div className="flex h-14 items-center justify-between dark:text-gray-900 md:hidden">
+        {defaultNavbar}
       </div>
     </nav>
   )
