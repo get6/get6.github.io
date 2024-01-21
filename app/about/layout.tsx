9feccda796219edb8b8a8ba71de25837d8cc0e69
get6 <ittae.com@gmail.com>
2024-01-21 17:46:54 +0900

    Update background image

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -5,7 +5,7 @@ export default function AboutLayout({
 }) {
   return (
     <>
-      <div className="fixed left-0 top-0 -z-10 h-full w-full bg-[url('/images/bg.jpg')] bg-cover bg-bottom bg-no-repeat" />
+      <div className="fixed left-0 top-0 -z-10 h-full w-full bg-[url('/images/about/bg.jpg')] bg-cover bg-bottom bg-no-repeat dark:bg-slate-900 dark:bg-[url('/images/about/dark-bg.jpg')] dark:bg-center" />
       {children}
     </>
   )
