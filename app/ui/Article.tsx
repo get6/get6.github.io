cdb274fefd62cfe449c1008e959d73a04c37298d
get6 <ittae.com@gmail.com>
2024-04-16 10:02:38 +0900

    style: code overflow

--- app/ui/Article.tsx
+++ app/ui/Article.tsx
@@ -1,7 +1,7 @@
 export default function Article({ html }: { html: string }) {
   return (
     <article
-      className="prose py-4 dark:prose-invert prose-headings:text-inherit prose-a:text-blue-600 prose-img:mx-auto prose-img:rounded-xl"
+      className="prose py-4 dark:prose-invert prose-headings:text-inherit prose-a:text-blue-600 prose-pre:max-w-[85vw] prose-img:mx-auto prose-img:rounded-xl md:prose-pre:max-w-none"
       dangerouslySetInnerHTML={{ __html: html }}
     />
   )
