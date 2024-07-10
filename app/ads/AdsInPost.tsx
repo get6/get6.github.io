ee4b59d841d3681dc0a388f66d7b6301c087437c
get6 <ittae.com@gmail.com>
2024-07-10 18:13:36 +0900

    test: adsense

---
+++ app/ads/AdsInPost.tsx
@@ -0,0 +1,15 @@
+export const AdsInPost = () => {
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{
+        display: 'block',
+        textAlign: 'center',
+      }}
+      data-ad-layout="in-article"
+      data-ad-format="fluid"
+      data-ad-client="ca-pub-1411731962767238"
+      data-ad-slot="6140930882"
+    ></ins>
+  )
+}
--- app/ui/Article.tsx
+++ app/ui/Article.tsx
@@ -1,8 +1,13 @@
+import { AdsInPost } from '@/app/ads/AdsInPost'
+
 export default function Article({ html }: { html: string }) {
   return (
-    <article
-      className={`prose my-4 dark:prose-invert prose-a:text-blue-600 prose-pre:max-w-[85vw] prose-table:table-fixed prose-th:text-start prose-th:align-top prose-td:text-pretty prose-td:break-words prose-img:mx-auto prose-img:my-0 prose-img:rounded-xl prose-img:shadow dark:prose-a:text-blue-500 md:prose-pre:max-w-none prose-a:no-underline`}
-      dangerouslySetInnerHTML={{ __html: html }}
-    />
+    <div className="my-4 gap-4">
+      <article
+        className={`prose dark:prose-invert prose-a:text-blue-600 prose-a:no-underline prose-pre:max-w-[85vw] prose-table:table-fixed prose-th:text-start prose-th:align-top prose-td:text-pretty prose-td:break-words prose-img:mx-auto prose-img:my-0 prose-img:rounded-xl prose-img:shadow dark:prose-a:text-blue-500 md:prose-pre:max-w-none`}
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+      <AdsInPost />
+    </div>
   )
 }
