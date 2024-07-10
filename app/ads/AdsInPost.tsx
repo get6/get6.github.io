d5b9f578467d88b761be0eb3673a5ebdaddebb9c
get6 <ittae.com@gmail.com>
2024-07-10 20:30:12 +0900

    style: update css to adpost

--- app/ads/AdsInPost.tsx
+++ app/ads/AdsInPost.tsx
@@ -9,16 +9,18 @@ export const AdsInPost = () => {
   }, [])
 
   return (
-    <ins
-      className="adsbygoogle max-w-prose"
-      style={{
-        display: 'block',
-        textAlign: 'center',
-      }}
-      data-ad-layout="in-article"
-      data-ad-format="fluid"
-      data-ad-client="ca-pub-1411731962767238"
-      data-ad-slot="6140930882"
-    ></ins>
+    <div className="max-w-prose px-6 lg:px-0">
+      <ins
+        className="adsbygoogle"
+        style={{
+          display: 'block',
+          textAlign: 'center',
+        }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-1411731962767238"
+        data-ad-slot="6140930882"
+      ></ins>
+    </div>
   )
 }
