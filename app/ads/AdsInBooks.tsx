c584e03f4208e93e562334549a290381a4664841
get6 <ittae.com@gmail.com>
2024-07-10 19:52:19 +0900

    feat: update ad style

--- app/ads/AdsInBooks.tsx
+++ app/ads/AdsInBooks.tsx
@@ -3,12 +3,12 @@ export const AdsInBooks = () => {
     <ins
       className="adsbygoogle"
       style={{
-        display: 'inline-block',
-        width: '886px',
-        height: '60px',
+        display: 'block',
       }}
       data-ad-client="ca-pub-1411731962767238"
       data-ad-slot="2705917050"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
     ></ins>
   )
 }
