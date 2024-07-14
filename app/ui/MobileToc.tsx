5745bbeb735f04b710075371875bb2d796d47815
get6 <ittae.com@gmail.com>
2024-07-14 11:50:10 +0900

    style: change mobile overflow

--- app/ui/MobileToc.tsx
+++ app/ui/MobileToc.tsx
@@ -12,7 +12,7 @@ export default function MobileToc({ headers }: Props) {
 
   return (
     headers && (
-      <nav className="h-fit w-full max-w-prose pt-6 xl:hidden">
+      <nav className="h-fit max-w-sm pt-6 md:max-w-none xl:hidden">
         <h2 className="mb-4 text-lg font-medium leading-6 text-gray-600 dark:text-gray-400">
           Table of Contents
         </h2>
