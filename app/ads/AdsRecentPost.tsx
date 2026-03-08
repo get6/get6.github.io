ca2d455fef48d992d86de78aa6b7a3be79c286d4
get6 <ittae.com@gmail.com>
2026-03-08 21:44:51 +0900

    Update ad slot in AdsRecentPost component for improved ad targeting

--- app/ads/AdsRecentPost.tsx
+++ app/ads/AdsRecentPost.tsx
@@ -27,7 +27,7 @@ export const AdsRecentPost = () => {
           data-ad-format="fluid"
           data-ad-layout-key="+37+pa+33+c+60"
           data-ad-client="ca-pub-1411731962767238"
-          data-ad-slot="9407374003"
+          data-ad-slot="7771446987"
         ></ins>
       )}
     </div>
